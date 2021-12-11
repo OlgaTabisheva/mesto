dbe242ea763e00f6416b4dae32d53d045f9f6f87
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2021-12-11 20:39:25 +0300

    Работа в процессе

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+
+
+const popupOpenButton = document.querySelector( '.profile__edit-button');
+const popupCloseButton = document.querySelector('.popup__close');
+const popup = document.querySelector( '.popup');
+
+
+function openPopup() {
+    popup.classList.add( 'popup_opened');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+popupOpenButton.addEventListener( 'click', openPopup);
+
+popupCloseButton.addEventListener ( 'click' , closePopup);
+
