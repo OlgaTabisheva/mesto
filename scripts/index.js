072da10ef57414132a847c5ac01dc9c6166b6035
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2021-12-18 18:07:53 +0300

    Работа в процессе

--- scripts/index.js
+++ scripts/index.js
@@ -6,29 +6,30 @@ const popup = document.querySelector( '.popup');
 
 function openPopup() {
     popup.classList.add( 'popup_opened');
+    let ProfileName = document.querySelector('.profile__name');
+    let ProfileJob = document.querySelector('.profile__job');
+    document.querySelector('.popup__name').value = ProfileName.textContent;
+    document.querySelector('.popup__job').value = ProfileJob.textContent;
 }
+
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
 function savePopup() {
     let formElement = document.querySelector( '.popup__container');
-    let ProfileName = document.querySelector('.profile__name');
-    let ProfileJob = document.querySelector('.profile__job');
-    console.log(ProfileName.textContent); // "Это текст внутри элемента."
-    console.log(ProfileJob.textContent); // "Это текст внутри элемента."
-
     function formSubmitHandler (evt) {
 
         evt.preventDefault();
-
         let nameInput = document.querySelector( '.popup__name');
         let jobInput = document.querySelector( '.popup__job');
+      ;
         document.querySelector('.profile__name').textContent = nameInput.value;
         document.querySelector('.profile__job').textContent = jobInput.value;
     }
 
     formElement.addEventListener('submit', formSubmitHandler);
+
     popup.classList.remove('popup_opened');
 }
 
@@ -36,9 +37,6 @@ function savePopup() {
 popupOpenButton.addEventListener( 'click', openPopup);
 popupCloseButton.addEventListener ( 'click' , closePopup);
 popupSaveButton.addEventListener ( 'click' , savePopup);
-
-
-
 
 
 
