de9b8275d510a41e66a81df7cf89c9e3cbfe49b3
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2021-12-18 16:26:19 +0300

    Работа в процессе

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,48 @@
 const popupOpenButton = document.querySelector( '.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
+const popupSaveButton = document.querySelector('.popup__save');
 const popup = document.querySelector( '.popup');
 
 
 function openPopup() {
     popup.classList.add( 'popup_opened');
 }
-
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-popupOpenButton.addEventListener( 'click', openPopup);
-popupCloseButton.addEventListener ( 'click' , closePopup);
+function savePopup() {
+    let formElement = document.querySelector( '.popup__container');
+    let ProfileName = document.querySelector('.profile__name');
+    let ProfileJob = document.querySelector('.profile__job');
+    console.log(ProfileName.textContent); // "Это текст внутри элемента."
+    console.log(ProfileJob.textContent); // "Это текст внутри элемента."
 
-let formElement = document.querySelector( '.popup__container');
+    function formSubmitHandler (evt) {
 
-function formSubmitHandler (evt) {
-     evt.preventDefault();
-    let nameInput = document.querySelector( '.popup__name');
-    let jobInput = document.querySelector( '.popup__job');
-    document.querySelector('.profile__name').textContent = nameInput.value;
-    document.querySelector('.profile__job').textContent = jobInput.value;
+        evt.preventDefault();
+
+        let nameInput = document.querySelector( '.popup__name');
+        let jobInput = document.querySelector( '.popup__job');
+        document.querySelector('.profile__name').textContent = nameInput.value;
+        document.querySelector('.profile__job').textContent = jobInput.value;
+    }
+
+    formElement.addEventListener('submit', formSubmitHandler);
+    popup.classList.remove('popup_opened');
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+
+popupOpenButton.addEventListener( 'click', openPopup);
+popupCloseButton.addEventListener ( 'click' , closePopup);
+popupSaveButton.addEventListener ( 'click' , savePopup);
+
+
+
+
+
+
+
+
+
+
