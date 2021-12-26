3e60bff4e85658f8e8ac74cd95e109e449cc0e1d
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2021-12-26 22:41:08 +0300

    Работа по 5 практической работе - корректировка

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,34 @@
-const popupOpenButton = document.querySelector( '.profile__edit-button');
+const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
-const popupSaveButton = document.querySelector('.button-save');
-const popup = document.querySelector( '.popup');
-
+const popup = document.querySelector('.popup');
+let ProfileName = document.querySelector('.profile__name');
+let ProfileJob = document.querySelector('.profile__job');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+let formElement = document.querySelector('.popup__form');
+nameInput.value = ProfileName.textContent;
+jobInput.value = ProfileJob.textContent;
 
 function openPopup() {
-    popup.classList.add( 'popup_opened');
-    let ProfileName = document.querySelector('.profile__name');
-    let ProfileJob = document.querySelector('.profile__job');
-    document.querySelector('.popup__name').value = ProfileName.textContent;
-    document.querySelector('.popup__job').value = ProfileJob.textContent;
+  popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
-function savePopup() {
-    let formElement = document.querySelector( '.popup__container');
-    function formSubmitHandler (evt) {
-
-        evt.preventDefault();
-        let nameInput = document.querySelector( '.popup__name');
-        let jobInput = document.querySelector( '.popup__job');
-      ;
-        document.querySelector('.profile__name').textContent = nameInput.value;
-        document.querySelector('.profile__job').textContent = jobInput.value;
-    }
-
-    formElement.addEventListener('submit', formSubmitHandler);
-
-    popup.classList.remove('popup_opened');
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  ProfileName.textContent = nameInput.value;
+  ProfileJob.textContent = jobInput.value;
+  closePopup();
 }
 
+formElement.addEventListener('submit', formSubmitHandler);
 
-popupOpenButton.addEventListener( 'click', openPopup);
-popupCloseButton.addEventListener ( 'click' , closePopup);
-popupSaveButton.addEventListener ( 'click' , savePopup);
+
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 
 
 
