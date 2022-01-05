36745011fe7da5446c14f8c81e51797a2ea88d86
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2022-01-05 13:48:02 +0300

    Работа по 6 практической работе - отправка на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -6,91 +6,62 @@ const editProfileButton = document.querySelector('.profile__edit-button');
 const addCardButton = document.querySelector('.profile__add-button');
 const editForm = editModal.querySelector('.popup__form');
 const addCardForm = addCardModal.querySelector('.popup__form');
-
+const viewCardModal = document.querySelector('.popup_type_image-container');
+const CloseViewCardModal = viewCardModal.querySelector('.popup__close');
 
 addCardForm.addEventListener('submit', (event) => {
   event.preventDefault()
   const nameValue = inputCardName.value;
   const linkValue = inputLink.value;
   document.placeInputForm.reset();
-
-  createCard({
-
+  renderCard({
     name: nameValue,
     link: linkValue
   })
-  toogleModal(addCardModal);
+  closePopup(addCardModal);
 });
 
 editForm.addEventListener('submit', (event) => {
   event.preventDefault()
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
-  toogleModal(editModal);
+  closePopup(editModal);
 
 });
 
 
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
 
 //инпуты
-let inputName = document.querySelector('.popup__input_type_name');
-let inputJob = document.querySelector('.popup__input_type_job');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputJob = document.querySelector('.popup__input_type_job');
 const inputCardName = document.querySelector(".popup__input_type_card-name");
 const inputLink = document.querySelector(".popup__input_card-link");
 
-
-function toogleModal(modal) {
-
-  modal.classList.toggle('popup_opened')
-
+function openPopup(modal) {
+  modal.classList.add('popup_opened');
 }
 
-function openPopupEdit() {
-  editModal.classList.add('popup_opened');
+function closePopup(modal) {
+  modal.classList.remove('popup_opened');
+}
+
+function openPopupEdit(editModal) {
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
+  openPopup(editModal);
 }
 
+editProfileButton.addEventListener('click', () => openPopupEdit(editModal))
+closeEditModal.addEventListener('click', () => closePopup(editModal))
 
-editProfileButton.addEventListener('click', () => openPopupEdit())
-closeEditModal.addEventListener('click', () => toogleModal(editModal))
-
-addCardButton.addEventListener('click', () => toogleModal(addCardModal))
-closeAddCardModalButton.addEventListener('click', () => toogleModal(addCardModal))
+addCardButton.addEventListener('click', () => openPopup(addCardModal))
+closeAddCardModalButton.addEventListener('click', () => closePopup(addCardModal))
+CloseViewCardModal.addEventListener('click', () => closePopup(viewCardModal))
 
 
-//CloseViewCardModal.addEventListener('click', () => toogleModal(viewCardModal))
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Санкт-Петербург',
-    link: 'https://images.unsplash.com/photo-1630354102354-2471c8851e60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-const list = document.querySelector(".elements");
+const cardsList = document.querySelector(".elements");
 const cardTemplate = document.querySelector('.card-template').content;
 
 //удаление карточки
@@ -98,50 +69,42 @@ function deleteHandler(e) {
   e.target.closest('.element').remove()
 }
 
-
 function createCard(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector('.element__image');
   const cardTitle = cardElement.querySelector('.element__group-title');
-  const deleteButton = cardElement.querySelector('.element__delete-button');
-  const likeButton = cardElement.querySelector('.element__group-heart');
-
-
+  const viewCardButton = cardElement.querySelector('.element__view-button');
   cardTitle.textContent = cardData.name;
   cardImage.src = `${cardData.link}`;
 
+  const deleteButton = cardElement.querySelector('.element__delete-button');
+  const likeButton = cardElement.querySelector('.element__group-heart');
+
+  function openPopupImage(viewCardModal){
+    document.querySelector('.popup__place-name').textContent = cardTitle.textContent;
+    document.querySelector('.popup__image-link').src = cardImage.src;
+    openPopup(viewCardModal)
+  }
 
   deleteButton.addEventListener("click", deleteHandler)
   likeButton.addEventListener("click", likeClickHandler)
+  viewCardButton.addEventListener('click', () => openPopupImage(viewCardModal))
 
   function likeClickHandler() {
     likeButton.classList.toggle('element__group-heart_active');
   }
+  cardsList.prepend(cardElement);
+  return cardElement;
 
-  list.prepend(cardElement);
-
-  function openPopup(viewCardModal) {
-    viewCardModal.classList.add('popup_opened');
-    document.querySelector('.popup__place-name').textContent = cardTitle.textContent;
-    document.querySelector('.popup__image-link').src = cardImage.src;
-  }
-
-// Функция закрытия viewCardModal реализована таким образом, т.к. внешний toogleModal на закрытие не работает.
-  function closePopup(viewCardModal) {
-    viewCardModal.classList.remove('popup_opened');
-  }
-
-  const viewCardModal = document.querySelector('.popup_type-image-container');
-  const viewCardButton = document.querySelector('.element__view-button');
-  const CloseViewCardModal = viewCardModal.querySelector('.popup__close');
-
-  viewCardButton.addEventListener('click', () => openPopup(viewCardModal))
-  CloseViewCardModal.addEventListener('click', () => closePopup(viewCardModal))
 }
 
-initialCards.forEach(createCard);
+function renderCard(cardData){
+    const cardElement = createCard(cardData);
+    cardsList.prepend(cardElement);
+
+   }
 
 
-//сделать третье модальное окно.
+initialCards.forEach(renderCard);
 
-//Плавное открытие и закрытие попапов
+
