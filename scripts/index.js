5f4f8511334a529f6aa1a380c8d727f2732c18e9
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2022-02-13 15:59:53 +0300

    Работа по 7 практической работе - создан класс для создания карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,32 @@
+import {Card} from './Card.js'
+import {FormValidator} from './FormValidator.js'
 
-import {FormValidator} from './validate.js'
-import {initialCards} from './initialArray.js'
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Санкт-Петербург',
+    link: 'https://images.unsplash.com/photo-1630354102354-2471c8851e60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 const editModal = document.querySelector(".popup_type_edit");
 const addCardModal = document.querySelector(".popup_type_add-card");
@@ -8,9 +34,9 @@ const editProfileButton = document.querySelector('.profile__edit-button');
 const addCardButton = document.querySelector('.profile__add-button');
 const editForm = editModal.querySelector('.popup__form');
 const addCardForm = addCardModal.querySelector('.popup__form');
-const viewCardModal = document.querySelector('.popup_type_image-container');
-const describePlace = document.querySelector('.popup__place-name')
-const describeLink = document.querySelector('.popup__image-link')
+export const viewCardModal = document.querySelector('.popup_type_image-container');
+export const describePlace = document.querySelector('.popup__place-name')
+export const describeLink = document.querySelector('.popup__image-link')
 
 addCardForm.addEventListener('submit', (event) => {
   event.preventDefault()
@@ -47,7 +73,7 @@ const inputJob = document.querySelector('.popup__input_type_job');
 const inputCardName = document.querySelector(".popup__input_type_card-name");
 const inputLink = document.querySelector(".popup__input_card-link");
 
-function openPopup(modal) {
+export function openPopup(modal) {
   document.addEventListener('keydown', closeByEscape)
   modal.classList.add('popup_opened');
 }
@@ -87,47 +113,15 @@ popups.forEach((popup) => {
 editProfileButton.addEventListener('click', () => openPopupEdit(editModal))
 addCardButton.addEventListener('click', () => openPopup(addCardModal))
 
-
 const cardsList = document.querySelector(".elements");
-const cardTemplate = document.querySelector('.card-template').content;
+ const cardTemplateSelector = '.card-template'
+ const cardTemplate = document.querySelector('.card-template').content;
 
-//удаление карточки
-function deleteHandler(e) {
-  e.target.closest('.element').remove()
-}
 
-function createCard(cardData) {
-  const cardElement = cardTemplate.cloneNode(true);
-  const cardImage = cardElement.querySelector('.element__image');
-  const cardTitle = cardElement.querySelector('.element__group-title');
-  const viewCardButton = cardElement.querySelector('.element__view-button');
-  cardTitle.textContent = cardData.name;
-  cardImage.src = `${cardData.link}`;
-  cardImage.alt = cardData.name;
 
-  const deleteButton = cardElement.querySelector('.element__delete-button');
-  const likeButton = cardElement.querySelector('.element__group-heart');
-
-  function openPopupImage(viewCardModal) {
-    describePlace.textContent = cardTitle.textContent;
-    describeLink.src = cardImage.src;
-    describeLink.alt = cardData.name;
-    openPopup(viewCardModal)
-  }
-
-  deleteButton.addEventListener("click", deleteHandler)
-  likeButton.addEventListener("click", likeClickHandler)
-  viewCardButton.addEventListener('click', () => openPopupImage(viewCardModal))
-
-  function likeClickHandler() {
-    likeButton.classList.toggle('element__group-heart_active');
-  }
-
-  return cardElement;
-}
-
-function renderCard(cardData) {
-  const cardElement = createCard(cardData);
+function renderCard(data, wrap) {
+  const card = new Card(data, cardTemplateSelector )
+  const cardElement = card.createCard()
   cardsList.prepend(cardElement);
 
 }
